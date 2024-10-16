a2e6575d995b21e3687cdfbb4fd9f72e15a2488b
Aryan Lall <aryanlall0712@gmail.com>
2024-10-17 00:22:23 +0530

    small change

--- app/main.py
+++ app/main.py
@@ -81,7 +81,7 @@ def main():
             continue
         else:
             error = True
-            print(f"[line {line}] Error: Unexpected character: {c}", file=sys.stderr)
+            print("[line %s] Error: Unexpected character: %s" % (line, c), file=sys.stderr,)
         i+=1
     print("EOF  null")
     if error:
