1d0b31fed9391e4ab4c39946976f0ca1f25e7595
Aryan Lall <aryanlall0712@gmail.com>
2024-10-16 20:58:28 +0530

    more equal

--- app/main.py
+++ app/main.py
@@ -40,8 +40,6 @@ def main():
             print("RIGHT_BRACE } null")
         elif c == ")":
             print("RIGHT_PAREN ) null")
-        elif c == "=":
-            print("EQUAL = null")
         elif c == "==":
             print("EQUAL_EQUAL == null")
         elif c == "!":
