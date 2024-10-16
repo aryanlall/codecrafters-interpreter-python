407eb4d96a34d693c16d53f951ef03b241fa303e
Aryan Lall <aryanlall0712@gmail.com>
2024-10-16 21:06:21 +0530

    once again

--- app/main.py
+++ app/main.py
@@ -41,9 +41,16 @@ def main():
         elif c == ")":
             print("RIGHT_PAREN ) null")
         elif c == "=":
-            print("EQUAL = null")
-        elif c == "==":
-            print("EQUAL_EQUAL == null")
+            if c == "=":
+                print("EQUAL_EQUAL == null")
+            else:
+                print("EQUAL = null")
+        elif c == "!":
+            print("NOT_EQUAL ! null")
+        elif c == "<":
+            print("LESS_EQUAL < null")
+        elif c == ">":
+            print("GREAT_EQUAL > null")
         else:
             error = True
             line_number = file_contents.count("\n", 0, file_contents.find(c)) + 1
